622bbb138e7d517795689d903f9ec7c8273f895a
inves <alphonserivoire@gmail.com>
2024-12-04 13:23:17 +0100

    update TCP

---
+++ src/UDP/UDPServerTest.java
@@ -0,0 +1,18 @@
+package UDP;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UDPServerTest {
+
+    @org.junit.jupiter.api.Test
+    void launch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
