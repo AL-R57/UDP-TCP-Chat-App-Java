6d2296800c2bb572c2e0a4c9fb14b8b64bf42d8e
AL-R57 <130043683+AL-R57@users.noreply.github.com>
2024-12-06 19:54:27 +0100

    Create UDPClientTest.java

---
+++ src/UDP/UDPClientTest.java
@@ -0,0 +1,16 @@
+package UDP;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UDPClientTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void main() {
+    }
+}
